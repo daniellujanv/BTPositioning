7767d1147684778d25153dd1eab904e4bb4f3b70
Daniel Lujan <dluj@itu.dk>
2013-10-08 10:06:22 +0200

    - clean data
    - gaussian filter
    - better formula to calculate distance

--- src/itu/pervasive/btpositioning/BtPositioning.java
+++ src/itu/pervasive/btpositioning/BtPositioning.java
@@ -1,13 +1,25 @@
 package itu.pervasive.btpositioning;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 
 public class BtPositioning extends Activity{
 
+	public List<Double> readings;
+	public List<Double> gaussianReadings;
+	public List<Double> maReadings; // moving average 
+	private double[] weights = new double[]{2, 2 ,2 , 3, 2, 2, 2};
+	private List<Double> distances;
+	
 	public BtPositioning(){
+		readings = new ArrayList<Double>();
+		gaussianReadings = new ArrayList<Double>();
+		distances = new ArrayList<Double>();
 	}
 	
-	public static double getDistance(double rssi_reading){
+	public double getDistance(double rssi_reading){
 		double n = 4;
 		double dBmi = rssi_reading;
 		double dBm0 = -63;
@@ -35,11 +47,74 @@ public class BtPositioning extends Activity{
 		 *  A^N = d
 		 * 
 		 */
-		N = (dBmi + dBm0 - PLM);
+		N = (dBmi - dBm0 - PLM);
 		N = N / (-10*n);
 		distance = Math.pow(A, N);
-		distance = distance/1000;
 		return distance;
 	}
 
+	public double addReading(double reading){
+		readings.add(reading);
+        double distance = getDistance(reading);
+        return distance;
+	}
+	
+	public void calculateDistances(){
+		GaussianFilter();
+		
+		for(int i=0; i< gaussianReadings.size();i++){
+			distances.add(getDistance(gaussianReadings.get(i)));
+		}
+	}
+	
+ 	private void GaussianFilter(){
+//		float[] temp = new float[readings.size()];
+		
+		double[] digitsAround = new double[weights.length];
+		
+		int offset = (weights.length / 2);
+		
+		int denominator = 0;
+		for(double r : weights){
+			denominator += r;
+		}
+		
+		for(int f = 0; f < readings.size(); f++){
+//			double numberToFilter = (double)readings.get(f);
+			//digitsAround = new float[weights.length];
+//			int index = 0;
+			
+			for(int e = 0; e <  weights.length; e++){
+//				int nextNumber = e;
+				//|| (e+f)+offset > readings.length
+				if((e+f) - offset < 0 || (e+f) - offset >= readings.size()){
+					digitsAround[e] = (double)readings.get(f);
+				}
+				else{
+					digitsAround[e] = (double)readings.get((e+f)-offset);
+				
+				}
+//				index++;
+			}
+			
+			float temporary = 0;
+			
+			for(int g = 0; g < digitsAround.length; g++){
+				temporary += digitsAround[g] * weights[g];
+			}
+			gaussianReadings.add((double)(temporary / denominator));
+			
+		}
+		
+//		return temp;
+	}
+
+ 	public String getCalculatedDistances(){
+ 		String response = null;
+ 		for(int i = 0; i < distances.size(); i++){
+ 			response += "\n init "+String.format("%.3f",readings.get(i))+", gauss_reading "
+ 					+String.format("%.3f",gaussianReadings.get(i))+" => "+ String.format("%.3f",distances.get(i));
+ 		}
+ 		return response;
+ 	}
 }
--- src/itu/pervasive/btpositioning/MainActivity.java
+++ src/itu/pervasive/btpositioning/MainActivity.java
@@ -1,5 +1,6 @@
 package itu.pervasive.btpositioning;
 
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,7 +11,9 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Looper;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -24,17 +27,19 @@ public class MainActivity extends Activity {
 	private TextView tv;
 	private BtPositioning btp;
 	private List<String> devices;
-	
+	private boolean goRead = true;
+	protected BtPositioning btPositioning;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		devices = new ArrayList<String>();
-		devices.add("54:26:96:D0:84:6E"); //daniel's macbook
+		//		devices.add("54:26:96:D0:84:6E"); //daniel's macbook
+		devices.add("08:37:3D:C7:2B:FD"); //galaxy_fame_758
 		tv = (TextView) findViewById(R.id.text);
 		tv.append("\n Searching for bt devices \n");
-
 		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 		if (mBluetoothAdapter == null) {
 			tv.append("initializeBt -> adapter null");
@@ -45,12 +50,9 @@ public class MainActivity extends Activity {
 				Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
 				startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
 			}else{
-//				while(true){
-					discoverDevices();
-//				}
+				new searchingManager().execute(null, null, null);
 			}
 		}	
-		tv.append("\n finished \n");
 	}
 
 	private void discoverDevices() {
@@ -61,6 +63,11 @@ public class MainActivity extends Activity {
 		registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
 	}
 
+	private void undiscoverDevices(){
+		unregisterReceiver(mReceiver);
+		Log.w("broadcastReceiver","UNregister");
+		mBluetoothAdapter.cancelDiscovery();
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -69,21 +76,21 @@ public class MainActivity extends Activity {
 		menu.add(0, 2, 0,"Repeat");
 		return true;
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-	    // Handle item selection
-	    switch (item.getItemId()) {
-	        case 2:
-	        	unregisterReceiver(mReceiver);
-	    		mBluetoothAdapter.cancelDiscovery();
-	    		discoverDevices();
-	        	return true;
-	        default:
-	            return super.onOptionsItemSelected(item);
-	    }
+		// Handle item selection
+		switch (item.getItemId()) {
+		case 2:
+			unregisterReceiver(mReceiver);
+			mBluetoothAdapter.cancelDiscovery();
+			discoverDevices();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
-	
+
 	public void onActivityResult(int requestCode, int resultCode, Intent data){
 		if(requestCode == REQUEST_ENABLE_BT){
 			if(resultCode == RESULT_OK){
@@ -94,35 +101,64 @@ public class MainActivity extends Activity {
 			}
 		}
 	}
-	
+
 	// Create a BroadcastReceiver for ACTION_FOUND
 	private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
-	    private List<String> mArrayAdapter = new ArrayList<String>();
-	    @Override		
-	    public void onReceive(Context context, Intent intent) {
-    		Log.w("broadcastReceiver","received");
-	        String action = intent.getAction();
-	        // When discovery finds a device
-	        if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-	            // Get the BluetoothDevice object from the Intent
-	            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-	        	if(devices.contains(device.getAddress()) ){
-		            short rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI, (short)0);
-		            // Add the name and address to an array adapter to show in a ListView
-		            double distance = BtPositioning.getDistance((double)rssi);
-		            mArrayAdapter.add(device.getName() + " rssi: "+ rssi + " -- dstnc: "+ String.format("%.6f",distance));
-		            tv.append(mArrayAdapter.get(mArrayAdapter.size()-1).toString()+"\n");	
-		    		unregisterReceiver(mReceiver);
-		    		Log.w("broadcastReceiver","UNregister");
-		    		mBluetoothAdapter.cancelDiscovery();
-		    		discoverDevices();
-	        	}
-//	        	else{
-//	        		tv.append("stranger - "+device.getAddress()+"\n");
-//	        	}
-	        }
-	    }
+		private List<String> mArrayAdapter = new ArrayList<String>();
+
+		@Override		
+		public void onReceive(Context context, Intent intent) {
+			Log.w("broadcastReceiver","received");
+			String action = intent.getAction();
+			// When discovery finds a device
+			if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+				// Get the BluetoothDevice object from the Intent
+				BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+				if(devices.contains(device.getAddress()) ){
+					short rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI, (short)0);
+					double distance = btPositioning.addReading((double)rssi);
+					mArrayAdapter.add(device.getName() + " rssi: "+ rssi + " -- dstnc: "+ String.format("%.6f",distance));
+					tv.append(device.getName() + " rssi: "+ rssi + " -- dstnc: "+ String.format("%.6f",distance)+"\n");
+					// Add the name and address to an array adapter to show in a ListView
+					undiscoverDevices();
+					if(btPositioning.readings.size() <= 5){
+						discoverDevices();
+					}else{
+						btPositioning.calculateDistances();
+						tv.append(btPositioning.getCalculatedDistances() + "\n*******EOR******\n");
+						goRead = false;
+					}
+
+				}
+			}
+		}
 	};
+
+	private class searchingManager extends AsyncTask<Void, Void, Void> {
+		protected Void doInBackground(Void... _void) {
+			Looper.prepare();
+			for(int i=0; i< 4; i++){
+				btPositioning = new BtPositioning();
+				discoverDevices();
+				while(goRead){
+					try {
+						Thread.sleep(3000);
+						if(goRead && btPositioning.readings.size() <= 5){//if goRead is true it means the readings.size() is still less than 6 and possibly frozen 
+							undiscoverDevices();
+							discoverDevices();
+						}
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				goRead = true;
+			}
+//			undiscoverDevices();
+			return null;
+		}
+	}
+
 
 	public void onDestroy(){
 		super.onDestroy();
