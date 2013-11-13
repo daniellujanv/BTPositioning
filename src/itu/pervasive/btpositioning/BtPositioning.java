ceab21d8233cacdd43765f566b1ac78319daf8df
Daniel Lujan <dluj@itu.dk>
2013-11-13 10:42:55 +0100

    - final version BtPositioning class

--- src/itu/pervasive/btpositioning/BtPositioning.java
+++ src/itu/pervasive/btpositioning/BtPositioning.java
@@ -4,117 +4,298 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
+import android.util.Log;
 
 public class BtPositioning extends Activity{
 
-	public List<Double> readings;
-	public List<Double> gaussianReadings;
+	public List<Double> readings; //raw readings
+	public List<Double> meanReadings; // readings after mean filter
 	public List<Double> maReadings; // moving average 
-	private double[] weights = new double[]{2, 2 ,2 , 3, 2, 2, 2};
-	private List<Double> distances;
-	
+	private double[] weights = new double[]{1 ,3 ,5 ,7 ,9 ,7 ,5 ,3 ,1};
+	public List<Double> distances; //distances for mean readings
+	public List<Double> avg_distances;
+	public double avg_distance = 0;
+	public String calculated_orientation = "Not Calculated";
+	public int orientation[]; //for displaying with ARActivity x = east|west y = north|south
+	public boolean accurate = true;
+
 	public BtPositioning(){
 		readings = new ArrayList<Double>();
-		gaussianReadings = new ArrayList<Double>();
+		meanReadings = new ArrayList<Double>();
 		distances = new ArrayList<Double>();
+		avg_distances = new ArrayList<Double>();
+		orientation = new int[]{0,0};
+		
+
+		orientation[0] = 0;
+		orientation[1] = -1;
+		avg_distance = 10.0;
 	}
-	
+
 	public double getDistance(double rssi_reading){
-		double n = 4;
 		double dBmi = rssi_reading;
-		double dBm0 = -63;
+		double dBm0 = 40;
 		double distance;
-		double PLM = 0; //4.3
+		double d0 = 1;
+		double n = 2.5;
 		double A = 10,N; //variables of formula to solve for X in LOG enpressions LogA(distance)=N
 		/**
-		 * 
 		 *	DBm(i) = DBm(0) + 10n*Log10(di/d0)
-		 *	n = Pt/Pr (Power transmitted / Power received)
-		 */
-//		dBmi = dBm0 + 10*n*Math.log10(distance);
-		/**
-		 *     
+		 *
 		 *     dBmi - dBm0
-		 *   [ ___________]= Log10(distance)
+		 *   [ ___________]= Log10(distance/d0)
 		 *        10*n
 		 * 
-		 *               N = LogA(distance)
+		 *   N = LogA(distance/d0)
 		 *       
 		 *    		dBmi - dBm0
-		 *   N =  [ ___________] , A = 10 , distance
+		 *   N =  [ ___________] , A = 10 , distance = d, d0 = initial distance
 		 *        		10*n
 		 * 
-		 *  A^N = d
-		 * 
+		 *  (A^N)*d0 = d
 		 */
-		N = (dBmi - dBm0 - PLM);
-		N = N / (-10*n);
-		distance = Math.pow(A, N);
+		if(Math.abs(dBmi) <= 77){ // 0-3 mts
+			n = 2.0;
+			d0 = 1;
+			dBm0 = 55;
+			//			Log.w("n","< 77");
+		}else 
+			/*	
+			if(Math.abs(dBmi) < 74){ //4 - 10 mts
+			n = 3.0;
+			d0 = 4;
+			dBm0 = -65;
+			Log.w("n","< 74");
+		}else 
+			 */
+		{
+			n = 4.0;
+			d0 = 13;
+			dBm0 = 76;
+			//			Log.w("n","> 77");
+		}
+
+		N = (Math.abs(dBmi) - dBm0);
+		N = N / (10*n);
+		distance = Math.pow(A, N)*d0;
 		return distance;
 	}
 
 	public double addReading(double reading){
 		readings.add(reading);
-        double distance = getDistance(reading);
-        return distance;
+		return getDistance(reading); 
 	}
-	
+
 	public void calculateDistances(){
-		GaussianFilter();
-		
-		for(int i=0; i< gaussianReadings.size();i++){
-			distances.add(getDistance(gaussianReadings.get(i)));
+		meanFilter();//first the mean filter is applied and the distances are calculated over that
+		meanFilter();//double filter
+		double avg_6 = 0.0;
+		for(int i=0; i< meanReadings.size();i++){
+			distances.add(getDistance(meanReadings.get(i)));
+			if(i%6 == 0 && i > 0){
+				avg_distances.add(avg_6/6);
+				avg_6 = 0;
+			}else{
+				avg_6 += distances.get(i);
+			}
 		}
 	}
+
+	public void calculateOrientation(){
+		/*
+		 * readings always start heading north
+		 * 				0
+		 * 				N
+		 * 		7		.		1
+		 * 			NW	.	NE
+		 * 				.
+		 *	6	W	.	.	.	E 2
+		 * 				.
+		 * 			SW	.	SE
+		 * 		5		.		3
+		 * 				S
+		 * 				4
+		 * Readings:
+		 * 00 - 05 = N
+		 * 06 - 11 = NE
+		 * 12 - 17 = E
+		 * 18 - 23 = SE
+		 * 24 - 29 = S
+		 * 30 - 35 = SW
+		 * 36 - 41 = W
+		 * 42 - 47 = NW
+		 *
+		 * If the highest reading points north the lowest must point south
+		 * 
+		 */
+		String[] orientations = new String[]{"North","North-East","East",
+				"South-East","South","South-West","West","North-West"};
+		//		String h_orientation;//string for heighest orientation
+		//		String l_orientation; //string for lowest orientation
+		int h_reading = 0; //index for heighest reading
+		int l_reading = 0; // index for lowest reading
+		accurate = true;
+		for(int i=0;i < distances.size() ; i++){
+			for(int j = i+1; j < distances.size()-1; j++){
+				if(distances.get(h_reading) < distances.get(j) ){
+					h_reading = j;
+				}
+				if(distances.get(l_reading) > distances.get(j)){
+					l_reading = j;
+				}
+			}
+		}
+		int index_h = (int) Math.floor(h_reading/6);
+		int index_l = (int) Math.floor(l_reading/6);
+		calculated_orientation = orientations[index_l];
+		//calculate index for orientation if index_l = 4 (south) 
+		//we need to know if index_h points to North
+		if( add(index_h, 4, orientations.length) == index_l || add(index_h, 3, orientations.length) == index_l || add(index_h, 5, orientations.length) == index_l){
+			accurate = true;
+		}else{
+			accurate = false;
+		}
+		
+		//calculate average distance with the 4 lowest
+		double average = 0.0;
+		average += distances.get(index_l); 
+		average += distances.get(add(index_l,-1, distances.size()));
+		average += distances.get(add(index_l,+1, distances.size()));
+		average += distances.get(add(index_l,+2, distances.size()));
+		average = average/4;
+		avg_distance = average;
+
+		/* x =  1 -> east
+		 * x = -1 -> west
+		 * y =  1 -> north
+		 * y = -1 -> south
+		 * 0 <= index_l <= 7
+		 */
+		switch(index_l){
+		case 0:
+			orientation[0] = 0;
+			orientation[1] = 1;
+			;
+			break;
+		case 1:
+			orientation[0] = 1;
+			orientation[1] = 1;
+			;
+			break;
+		case 2: 
+			orientation[0] = 1;
+			orientation[1] = 0;
+			;
+			break;
+		case 3: 
+			orientation[0] = 1;
+			orientation[1] = -1;
+			;
+			break;
+		case 4: 
+			orientation[0] = 0;
+			orientation[1] = -1;
+			;
+			break;
+		case 5:
+			orientation[0] = -1;
+			orientation[1] = -1;
+			;
+			break;
+		case 6: 
+			orientation[0] = -1;
+			orientation[1] = 0;
+			;
+			break;
+		case 7: 
+			orientation[0] = -1;
+			orientation[1] = 1;
+			;
+			break;
+		default: 
+			orientation[0] = 0;
+			orientation[1] = 0;
+			;
+			break;
+		}
+	}
+
+	private int add(int index, int toAdd, int listSize){
+		if(index + toAdd > listSize){
+			return (index + toAdd - 1 - listSize);
+		}else if(index + toAdd < 0){
+			return (toAdd + index + listSize);
+		}else{
+			return index + toAdd;
+		}
+	}
+
+	public int[] getOrientation(){
+		return orientation;
+	}
+
+	public String getSOrientation(){
+		return this.calculated_orientation;
+	}
 	
- 	private void GaussianFilter(){
-//		float[] temp = new float[readings.size()];
-		
+	public boolean getAccuracy(){
+		return accurate;
+	}
+	
+	private void meanFilter(){
+		//how many digits around the one being processed are taken into account
 		double[] digitsAround = new double[weights.length];
-		
-		int offset = (weights.length / 2);
-		
+		//digits to "include" if processing the initial/final part of list
+		int offset = (weights.length / 3);
 		int denominator = 0;
 		for(double r : weights){
 			denominator += r;
 		}
-		
-		for(int f = 0; f < readings.size(); f++){
-//			double numberToFilter = (double)readings.get(f);
-			//digitsAround = new float[weights.length];
-//			int index = 0;
-			
-			for(int e = 0; e <  weights.length; e++){
-//				int nextNumber = e;
-				//|| (e+f)+offset > readings.length
-				if((e+f) - offset < 0 || (e+f) - offset >= readings.size()){
-					digitsAround[e] = (double)readings.get(f);
+		meanReadings.clear();
+		meanReadings = new ArrayList<Double>();
+		for(int i = 0; i < readings.size(); i++){
+			for(int j = 0; j <  weights.length; j++){
+				if((i+j) - offset < 0 || (i+j) - offset >= readings.size()){
+					digitsAround[j] = (double)readings.get(i); // adding numbers around the list to complete mean filter
 				}
 				else{
-					digitsAround[e] = (double)readings.get((e+f)-offset);
-				
+					digitsAround[j] = (double)readings.get((i+j)-offset);
 				}
-//				index++;
 			}
-			
 			float temporary = 0;
-			
+			//multiply by weights
 			for(int g = 0; g < digitsAround.length; g++){
 				temporary += digitsAround[g] * weights[g];
 			}
-			gaussianReadings.add((double)(temporary / denominator));
-			
+			//get smoothed number
+			meanReadings.add((double)(temporary / denominator));
 		}
-		
-//		return temp;
+		readings.clear();
+		readings = new ArrayList<Double>(meanReadings);
 	}
 
- 	public String getCalculatedDistances(){
- 		String response = null;
- 		for(int i = 0; i < distances.size(); i++){
- 			response += "\n init "+String.format("%.3f",readings.get(i))+", gauss_reading "
- 					+String.format("%.3f",gaussianReadings.get(i))+" => "+ String.format("%.3f",distances.get(i));
- 		}
- 		return response;
- 	}
+	public String getCalculatedDistances(){
+		String response = "****\n Readings + Distances \n";
+		for(int i = 0; i < readings.size(); i++){
+			response += (i+1)+ "reading "+ String.format("%.3f",readings.get(i)) 
+					+ " distance - "+ String.format("%.3f",distances.get(i)) + " \n";
+		}
+		response += "Average " + String.format("%.3f", avg_distance)+"\n";
+		response += "Orientation " + getSOrientation();
+		Log.w("ALL", response);
+		return response;
+	}
+
+	public double getCalculatedDistance(){
+		return avg_distance;
+	}
+
+	public void printList(List<Double> list, String it){
+		String toPrint = it;
+		for(int i=0;i<list.size();i++){
+			toPrint += ","+list.get(i).toString();
+		}
+		toPrint+="\n";
+		Log.w("mean",toPrint);
+	}
 }
--- src/itu/pervasive/btpositioning/MainActivity.java
+++ src/itu/pervasive/btpositioning/MainActivity.java
@@ -1,6 +1,5 @@
 package itu.pervasive.btpositioning;
 
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,13 +10,20 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Looper;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -25,107 +31,195 @@ public class MainActivity extends Activity {
 	private int REQUEST_ENABLE_BT = 1;
 
 	private TextView tv;
-	private BtPositioning btp;
+	private ScrollView sv;
 	private List<String> devices;
 	private boolean goRead = true;
-	protected BtPositioning btPositioning;
-
+	//	protected BtPositioning btPositioning;
+	private BtPositioning btPositioningAll;
+	private SearchingManager searchManager;
+	private ProgressBar wholeProgress;
+	private ProgressBar individualProgress;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		devices = new ArrayList<String>();
-		//		devices.add("54:26:96:D0:84:6E"); //daniel's macbook
 		devices.add("08:37:3D:C7:2B:FD"); //galaxy_fame_758
+		//find views
 		tv = (TextView) findViewById(R.id.text);
-		tv.append("\n Searching for bt devices \n");
+		sv = (ScrollView) findViewById(R.id.scrollview);
+		wholeProgress = (ProgressBar) findViewById(R.id.wholeprogressbar);
+		individualProgress = (ProgressBar) findViewById(R.id.individualprogressbar);
+
+		tv.setText("\n Searching for bt devices \n");
+		//initialize BT adapter
 		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-		if (mBluetoothAdapter == null) {
-			tv.append("initializeBt -> adapter null");
-			// Device does not support Bluetooth
+		makeToast("starting");
+
+		//manager for searching devices
+		searchManager = new SearchingManager(); 
+		if (mBluetoothAdapter == null) { // Device does not support Bluetooth
+			appendScreen("Device does not support Bluetooth");
+		}else if (!mBluetoothAdapter.isEnabled()) {//bluetooth is not enabled
+			appendScreen("initializing BT");
+			Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+			startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
 		}else{
-			if (!mBluetoothAdapter.isEnabled()) {
-				tv.append("initializeBt -> bt not enabled");
-				Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-				startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-			}else{
-				new searchingManager().execute(null, null, null);
-			}
-		}	
+			//everything good, search for devices
+			searchManager.execute(null, null, null); 
+		}
 	}
 
+
+	/*
+	 * start discovery of devices
+	 * initialize intent for ACTION_FOUND
+	 * register receiver for intent
+	 * 
+	 */
 	private void discoverDevices() {
-		// TODO Auto-generated method stub
-		Log.w("discoverDevices","register");
+		//		Log.w("discoverDevices","register");
 		mBluetoothAdapter.startDiscovery();
 		IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-		registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
+		registerReceiver(mReceiver, filter);
 	}
 
+	/*
+	 * stop discovering devices
+	 * unregister receiver for intent ACTION_FOUND
+	 * cancelDiscovery on bluetoothAdapter
+	 * 
+	 */
 	private void undiscoverDevices(){
-		unregisterReceiver(mReceiver);
-		Log.w("broadcastReceiver","UNregister");
+		//		Log.w("broadcastReceiver","UNregister");
+		try{
+			unregisterReceiver(mReceiver);
+		}catch(Exception e){}
 		mBluetoothAdapter.cancelDiscovery();
 	}
 
+	/*
+	 * 
+	 * simple method to display something on screen with TOAST
+	 */
+	private void makeToast(String toPrint) {
+		Toast.makeText(getApplicationContext(), toPrint, Toast.LENGTH_SHORT).show();
+	}
+
+	/*
+	 * beep sound 
+	 */
+	private void beep() {
+		try {
+			Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+			Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
+			r.play();
+		} catch (Exception e) {}
+	}
+
+	/*
+	 * 
+	 * append text to TV in screen - to be deleted
+	 */
+	private void appendScreen(String text){
+		tv.append(text);
+		sv.fullScroll(View.FOCUS_DOWN);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
+	 * create menu options
+	 */
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
-		menu.add(0, 2, 0,"Repeat");
+		menu.add(0, 2, 0,"Repeat Search!");
 		return true;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see android.app.Activity#onOptionsItemSelected(android.view.MenuItem)
+	 * options in menu
+	 */
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// Handle item selection
 		switch (item.getItemId()) {
 		case 2:
-			unregisterReceiver(mReceiver);
-			mBluetoothAdapter.cancelDiscovery();
-			discoverDevices();
+			searchManager.execute(null, null, null); 
 			return true;
 		default:
 			return super.onOptionsItemSelected(item);
 		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see android.app.Activity#onDestroy()
+	 * destroy method, unregister everything if needed
+	 * disable bluetooth adapter
+	 * cancel asynctask searchManager
+	 */
+	public void onDestroy(){
+		super.onDestroy();
+		try{
+			Log.w("destroy","destroooy");
+			searchManager.cancel(true);
+			unregisterReceiver(mReceiver);
+			mBluetoothAdapter.cancelDiscovery();
+			mBluetoothAdapter.disable();			
+		}catch(Exception e){
+			Log.w("onDestroy", e.toString());
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see android.app.Activity#onActivityResult(int, int, android.content.Intent)
+	 * Intent for enabling Bluetooth
+	 */
 	public void onActivityResult(int requestCode, int resultCode, Intent data){
 		if(requestCode == REQUEST_ENABLE_BT){
 			if(resultCode == RESULT_OK){
 				// Register the BroadcastReceiver
-				discoverDevices();
+				searchManager.execute(null, null, null);
 			}else {
-				tv.append("result NOT_OK - request_enable_bt \n");
+				appendScreen("result NOT_OK - request_enable_bt \n");
+				Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+				startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
 			}
 		}
 	}
 
-	// Create a BroadcastReceiver for ACTION_FOUND
+	/*
+	 *  Create a BroadcastReceiver for ACTION_FOUND
+	 *  when device is found it is processed if it is in the list
+	 *  of known devices - NOT ON PAIRED DEVICES
+	 */
 	private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
-		private List<String> mArrayAdapter = new ArrayList<String>();
-
 		@Override		
 		public void onReceive(Context context, Intent intent) {
-			Log.w("broadcastReceiver","received");
 			String action = intent.getAction();
 			// When discovery finds a device
 			if (BluetoothDevice.ACTION_FOUND.equals(action)) {
 				// Get the BluetoothDevice object from the Intent
 				BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
 				if(devices.contains(device.getAddress()) ){
+					//					Log.w("broadcastReceiver","received");
 					short rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI, (short)0);
-					double distance = btPositioning.addReading((double)rssi);
-					mArrayAdapter.add(device.getName() + " rssi: "+ rssi + " -- dstnc: "+ String.format("%.6f",distance));
-					tv.append(device.getName() + " rssi: "+ rssi + " -- dstnc: "+ String.format("%.6f",distance)+"\n");
-					// Add the name and address to an array adapter to show in a ListView
+					btPositioningAll.addReading((double)rssi);
+					//					Log.w("device",device.getName() + " rssi: "+ rssi + " -- dstnc: "+ String.format("%.6f",distance)+"\n");
 					undiscoverDevices();
-					if(btPositioning.readings.size() <= 5){
+					if((btPositioningAll.readings.size() % 6) != 0){ //taking 6 readings on each side
 						discoverDevices();
+						goRead = true;
 					}else{
-						btPositioning.calculateDistances();
-						tv.append(btPositioning.getCalculatedDistances() + "\n*******EOR******\n");
+						//						btPositioning.calculateDistances();//calculating distances & applying mean filter for every 8 readings
+						//						appendScreen(btPositioning.getCalculatedDistances() + "\n*************\n");
 						goRead = false;
 					}
 
@@ -134,35 +228,101 @@ public class MainActivity extends Activity {
 		}
 	};
 
-	private class searchingManager extends AsyncTask<Void, Void, Void> {
-		protected Void doInBackground(Void... _void) {
+	/*
+	 * Manager fo Bluetooth devices search 
+	 * puts thread to sleep 3 seconds after reading 6 RSSIs
+	 */
+	private class SearchingManager extends AsyncTask<Void, Integer, Integer> {
+		private long initialTime;
+		private long endingTime;
+		protected Integer doInBackground(Void... _void) {
+			//			Log.w("ready", "searchingManagerStarted");
 			Looper.prepare();
-			for(int i=0; i< 4; i++){
-				btPositioning = new BtPositioning();
-				discoverDevices();
-				while(goRead){
-					try {
+			initialTime = System.currentTimeMillis();
+			try {
+				btPositioningAll = new BtPositioning();
+				for(int i=0; i< 8; i++){
+					if(i != 0){
+						makeToast("Turn 45 degrees to the right");
+					}
+					publishProgress(i+1);
+					Log.w("run", ""+(i+1));
+					Thread.sleep(3000);
+					//					btPositioning = new BtPositioning();
+					discoverDevices();
+					while(goRead){
 						Thread.sleep(3000);
-						if(goRead && btPositioning.readings.size() <= 5){//if goRead is true it means the readings.size() is still less than 6 and possibly frozen 
-							undiscoverDevices();
+						if(goRead && btPositioningAll.readings.size() < 48){//if goRead is true it means the readings.size() is still less than 6 and possibly frozen 
+							try{
+								undiscoverDevices();								
+							}catch(Exception e){}
 							discoverDevices();
 						}
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
+						int wholeprogress = ((i*100)/8);
+						int individualprogress = (int)((((double)btPositioningAll.readings.size()%6)/6)*100);
+						publishProgress(-1, wholeprogress,individualprogress);
 					}
+					goRead = true;
 				}
-				goRead = true;
+				//				btPositioningAll.calculateDistances();//calculating distances & applying mean filter for all the readings
+				//				btPositioningAll.calculateOrientation();
+				publishProgress(1000);
+				endingTime = System.currentTimeMillis();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
-//			undiscoverDevices();
 			return null;
 		}
-	}
+
+		protected void onProgressUpdate(Integer... progress){
+			if(progress[0] < 0){
+				wholeProgress.setProgress(progress[1]);
+				individualProgress.setProgress(progress[2]);
+				Log.w("progress", "ind:"+ progress[2] + " whole:"+ progress[1]);
+			}else if(progress[0] == 1000){				
+				wholeProgress.setProgress(100);
+				beep();
+				beep();
+			}else {
+				appendScreen("\n RUN -> #"+progress[0]+"\n");
+				beep();
+			}
+
+		}
 
 
-	public void onDestroy(){
-		super.onDestroy();
-		Log.w("destroy","destroooy");
-		unregisterReceiver(mReceiver);
+		protected void onPostExecute(final Integer result){
+
+			btPositioningAll.calculateDistances();//calculating distances & applying mean filter for all the readings
+			btPositioningAll.calculateOrientation();
+			appendScreen(btPositioningAll.getCalculatedDistances() + "\n******DONE*******\n");
+			appendScreen("time of run(mins): "+(endingTime-initialTime)/60000);//minutes
+			Log.w("runningTime","time of run(mins): "+(endingTime-initialTime)/60000);//minutes
+
+			//		device.setDistance(btPositioningAll.getCalculatedDistance());
+			//		device.setIOrientation(btPositioningAll.getOrientation());
+			Log.w("onPostExecute", "finished 8 readings");
+			//		toast(btPositioningAll.getCalculatedDistances() + "\n "
+			//				+ btPositioningAll.getOrientation() +" \n******DONE*******\n",true, false);
+
+		}
 	}
 }
+
+
+/*
+ *we always start heading north
+ * 
+ * 				N
+ * 				.
+ * 			wn	.	ne
+ * 				.
+ *		W	.	.	.	E
+ * 				.
+ * 			sw	.	se
+ * 				.
+ * 				S
+ * 
+ * 
+ * 
+ */
